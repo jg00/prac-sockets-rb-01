a4909f7271a6fb66bc2114e82ab8a20592343c83
Jack Gamboa <jgamboa929@gmail.com>
2020-01-09 10:18:56 -0600

    Backup simple chat app.

---
+++ 03-socketio201/socketio201/chat.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const app = express();
+const socketio = require("socket.io"); // returns Socket constructor function
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, "public")));
+
+const expressServer = app.listen(9000, () => console.log("Server started!"));
+
+const io = new socketio(expressServer); // io is our server (server.sockets for example our io.sockets)
+
+// Reference for another way to setting up our Socket (ie io) server
+// const io = socketid(); // Gives us a server but we need to attach and hand it our http server
+// io.attach(expressServer)
+
+// Reference for setting defaults.  Also new keyword can be used.
+// const io = socketio(expressServer, {
+//   path: "/socket.io",
+//   serveClient: true,
+//   wsEngine: "ws"
+// });
+
+// Fired upon connection from a client specifically a connection to a namespace.
+// io is our socket server but the client still needs to connect to a namespace (represents a pool of sockets).
+io.on("connection", socket => {
+  socket.emit(
+    "messageFromServer",
+    { data: "Welcome to the socketio server" }, // data sent to client.
+    () => console.log("Acknowledged the client got the message!") // cb called by client to acknowledge data received.
+  );
+
+  socket.on("dataToServer", dataFromClient => {
+    console.log(dataFromClient);
+  });
+
+  socket.on("newMessageToServer", msg => {
+    // console.log(msg);
+    io.emit("messageToClients", { text: msg.text });
+  });
+});
