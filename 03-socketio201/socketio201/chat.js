be9303d0725609e123143108c6a7a229ca9448db
Jack Gamboa <jgamboa929@gmail.com>
2020-01-18 08:20:35 -0600

    Set up client and server multiple namespaces.

--- 03-socketio201/socketio201/chat.js
+++ 03-socketio201/socketio201/chat.js
@@ -1,12 +1,13 @@
 const express = require("express");
 const app = express();
-const socketio = require("socket.io"); // returns Socket constructor function
+const socketio = require("socket.io"); // returns Socket constructor function; This is our Socket Server
 const path = require("path");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(9000, () => console.log("Server started!"));
 
+// Invoke our Socket server.
 const io = new socketio(expressServer); // io is our server (server.sockets for example our io.sockets)
 
 // Reference for another way to setting up our Socket (ie io) server
@@ -22,6 +23,7 @@ const io = new socketio(expressServer); // io is our server (server.sockets for 
 
 // Fired upon connection from a client specifically a connection to a namespace.
 // io is our socket server but the client still needs to connect to a namespace (represents a pool of sockets).
+// io.on = io.of('/').on()  <- These are exactly the same.
 io.on("connection", socket => {
   socket.emit(
     "messageFromServer",
@@ -35,6 +37,30 @@ io.on("connection", socket => {
 
   socket.on("newMessageToServer", msg => {
     // console.log(msg);
-    io.emit("messageToClients", { text: msg.text });
+    // io.emit("messageToClients", { text: msg.text }); // This is going to the main namespace '/' by default.
+    io.of("/").emit("messageToClients", { text: msg.text }); // This is going to the main namespace '/' by default.
   });
+
+  /*
+    The server can still communicate across namespaces
+    but on the client, the socket needs to be in THAT namespace
+    in order to get the events.
+
+    It is important to note that the client may first connect to the main '/' namespace before
+    they connect to the next namespace '/admin'.
+    So below is being sent out before the client is actually connected to the '/admin' namespace.
+    Just remember connections have to be established first.
+  */
+  setTimeout(() => {
+    io.of("/admin").emit(
+      "welcome",
+      "Welcome to the admin channel from the main channel!"
+    );
+  }, 2000);
+});
+
+// "socket" parameter below is just what we are calling the thing that just connected.
+io.of("/admin").on("connection", socket => {
+  console.log("Someone connected to the admin namespace");
+  io.of("/admin").emit("welcome", "Welcome to the admin channel!");
 });
