16b10e9d30d0ab130a37bb99c6f90e15edf48e1e
Jack Gamboa <jgamboa929@gmail.com>
2020-01-18 14:10:57 -0600

    Test room setup and broadcast messages.

--- 03-socketio201/socketio201/chat.js
+++ 03-socketio201/socketio201/chat.js
@@ -1,65 +1,33 @@
+const path = require("path");
 const express = require("express");
 const app = express();
-const socketio = require("socket.io"); // returns Socket constructor function; This is our Socket Server
-const path = require("path");
+const socketio = require("socket.io");
 
 app.use(express.static(path.join(__dirname, "public")));
 
 const expressServer = app.listen(9000, () => console.log("Server started!"));
 
-// Invoke our Socket server.
-const io = new socketio(expressServer); // io is our server (server.sockets for example our io.sockets)
+const io = new socketio(expressServer); // Invoke our Socket server object.
 
-// Reference for another way to setting up our Socket (ie io) server
-// const io = socketid(); // Gives us a server but we need to attach and hand it our http server
-// io.attach(expressServer)
-
-// Reference for setting defaults.  Also new keyword can be used.
-// const io = socketio(expressServer, {
-//   path: "/socket.io",
-//   serveClient: true,
-//   wsEngine: "ws"
-// });
-
-// Fired upon connection from a client specifically a connection to a namespace.
-// io is our socket server but the client still needs to connect to a namespace (represents a pool of sockets).
-// io.on = io.of('/').on()  <- These are exactly the same.
+// io.on() equivalent to io.of('/').on()
 io.on("connection", socket => {
-  socket.emit(
-    "messageFromServer",
-    { data: "Welcome to the socketio server" }, // data sent to client.
-    () => console.log("Acknowledged the client got the message!") // cb called by client to acknowledge data received.
-  );
+  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
 
   socket.on("dataToServer", dataFromClient => {
     console.log(dataFromClient);
   });
 
-  socket.on("newMessageToServer", msg => {
-    // console.log(msg);
-    // io.emit("messageToClients", { text: msg.text }); // This is going to the main namespace '/' by default.
-    io.of("/").emit("messageToClients", { text: msg.text }); // This is going to the main namespace '/' by default.
-  });
+  socket.join("level1");
 
-  /*
-    The server can still communicate across namespaces
-    but on the client, the socket needs to be in THAT namespace
-    in order to get the events.
+  socket
+    .to("level1")
+    .emit("joined", `${socket.id} says have joined the level 1 room!`);
 
-    It is important to note that the client may first connect to the main '/' namespace before
-    they connect to the next namespace '/admin'.
-    So below is being sent out before the client is actually connected to the '/admin' namespace.
-    Just remember connections have to be established first.
-  */
-  setTimeout(() => {
-    io.of("/admin").emit(
-      "welcome",
-      "Welcome to the admin channel from the main channel!"
-    );
-  }, 2000);
+  // io.of("/")
+  //   .to("level1")
+  //   .emit("joined", `${socket.id} says have joined the level 1 room!`);
 });
 
-// "socket" parameter below is just what we are calling the thing that just connected.
 io.of("/admin").on("connection", socket => {
   console.log("Someone connected to the admin namespace");
   io.of("/admin").emit("welcome", "Welcome to the admin channel!");
--- 03-socketio201/socketio201/public/scripts.js
+++ 03-socketio201/socketio201/public/scripts.js
@@ -1,35 +1,18 @@
-/* Remember the 'client' makes requests to join the namespace */
-const socket = io("http://localhost:9000"); // 'http://localhost:9000' is the '/' namespace/endpoint
-const socket2 = io("http://localhost:9000/admin"); // /admin namespace.  Remember this is not really an endpoint.  Namespace is a feature internal for Socket.io for internal management.
-// const socket3 = io("http://localhost:9000/marketing"); // marketing namespace.
-
-console.log(socket.io); // We have our socket object
-console.log(socket.id); // undefined 1 b/c JS is async and continues execution of this line, 2 the connection is not completed yet at this point.  So to get this you can place inside socket.on('connect')
-
-// Need to place properties of socket like .id inside of 'connect'
-socket.on("connect", () => {
-  console.log(socket.id); // Now we get A76NXfqnAyfRRAkTAAAC id
-});
-
-// There is no cross socket communication.
-socket2.on("connect", () => {
-  console.log(socket2.id); // Now we get /admin#A76NXfqnAyfRRAkTAAAC
-});
-
-// Will never fire because the server is not emitting any 'welcome' events to the '/' namespace 'unless' we do so.
-socket.on("welcome", msg => {
-  console.log(msg);
-});
-
-// Only this will fire from server issuing 'welcome' events from the '/admin namespace.
-socket2.on("welcome", msg => {
-  console.log(msg);
-});
+const socket = io("http://localhost:9000"); // / namespace/endpoint
+const socket2 = io("http://localhost:9000/admin"); // /admin namespace
 
 socket.on("messageFromServer", (dataFromServer, ack) => {
   console.log(dataFromServer);
-  ack();
+  // ack();
   socket.emit("dataToServer", { data: "This is from the client" });
+});
+
+socket.on("joined", msg => {
+  console.log(msg);
+});
+
+socket2.on("welcome", dataFromServer => {
+  console.log(dataFromServer);
 });
 
 document.querySelector("#message-form").addEventListener("submit", event => {
@@ -37,19 +20,3 @@ document.querySelector("#message-form").addEventListener("submit", event => {
   const newMessage = document.querySelector("#user-message").value;
   socket.emit("newMessageToServer", { text: newMessage });
 });
-
-socket.on("messageToClients", msg => {
-  console.log(msg);
-  document.querySelector("#messages").innerHTML += `<li>${msg.text}</li>`;
-});
-
-/* Reference only
-  socket.on("ping", () => {
-    console.log("Ping was received from the server");
-  });
-
-  socket.on("pong", latency => {
-    console.log(latency);
-    console.log("Pong was sent to the server");
-  });
-*/
