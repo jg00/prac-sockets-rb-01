716437f914a3d2e92e5b7b32b67c7a8b5cd59830
Jack Gamboa <jgamboa929@gmail.com>
2020-01-29 20:52:50 -0600

    Set up namespaces, send to list, and update the DOM.

--- 03-socketio201/socketio201-slack/slack/public/scripts.js
+++ 03-socketio201/socketio201-slack/slack/public/scripts.js
@@ -1,22 +1,52 @@
-const socket = io("http://localhost:9000"); // / namespace/endpoint
-const socket2 = io("http://localhost:9000/admin"); // /admin namespace
+const socket = io("http://localhost:9000"); // Connect to / ie "main" namespace/endpoint
+
+console.log(socket.io);
+socket.on("connect", () => {
+  console.log(socket.id);
+});
+
+// listen for nsList which is a list of all the namespaces.
+socket.on("nsList", nsData => {
+  console.log("The list of namespaces has arrived");
+  console.log(nsData);
+
+  // Take the list and update the DOM
+  let namespacesDiv = document.querySelector(".namespaces");
+  namespacesDiv.innerHTML = "";
+  nsData.forEach(ns => {
+    namespacesDiv.innerHTML += `<div class="namespace" ns=${ns.endpoint}><img src="${ns.img}"</img></div>`;
+  });
+
+  // Add a click listener for each namespace
+  // console.log(document.getElementsByClassName("namespace"));
+  // console.log(document.querySelectorAll(".namespace"));
+
+  Array.from(document.getElementsByClassName("namespace")).forEach(elem => {
+    // console.log(elem);
+    elem.addEventListener("click", e => {
+      // console.dir(e.target);
+      const nsEndpoint = elem.getAttribute("ns");
+      console.log(`${nsEndpoint} I should go to now`);
+
+      // Next we want to join the namespace when clicked.
+      // CONTINUE HERE.
+    });
+  });
+});
 
 socket.on("messageFromServer", (dataFromServer, ack) => {
   console.log(dataFromServer);
   // ack();
-  socket.emit("dataToServer", { data: "This is from the client" });
-});
-
-socket.on("joined", msg => {
-  console.log(msg);
-});
-
-socket2.on("welcome", dataFromServer => {
-  console.log(dataFromServer);
+  socket.emit("dataToServer", { data: "Data from the client" });
 });
 
 document.querySelector("#message-form").addEventListener("submit", event => {
   event.preventDefault();
   const newMessage = document.querySelector("#user-message").value;
   socket.emit("newMessageToServer", { text: newMessage });
+});
+
+socket.on("messageToClients", msg => {
+  console.log(msg);
+  document.querySelector("#messages").innerHTML += `<li>${msg.text}</li>`;
 });
--- 03-socketio201/socketio201-slack/slack/slack.js
+++ 03-socketio201/socketio201-slack/slack/slack.js
@@ -4,7 +4,7 @@ const app = express();
 const socketio = require("socket.io");
 
 const namespaces = require("./data/namespaces");
-console.log(namespaces);
+// console.log(namespaces[0]);
 
 app.use(express.static(path.join(__dirname, "public")));
 
@@ -12,26 +12,28 @@ const expressServer = app.listen(9000, () => console.log("Server started!"));
 
 const io = new socketio(expressServer); // Invoke our Socket server object.
 
-// io.on() equivalent to io.of('/').on()
+// io.on() equivalent to io.of('/').on(); "Main" namespace.
 io.on("connection", socket => {
-  socket.emit("messageFromServer", { data: "Welcome to the socketio server" });
+  // console.log("Main namespace", socket);
 
-  socket.on("dataToServer", dataFromClient => {
-    console.log(dataFromClient);
+  // build an array to send back with the img and endpoint for each namespace
+  let nsData = namespaces.map(ns => {
+    return {
+      img: ns.img,
+      endpoint: ns.endpoint
+    };
   });
 
-  socket.join("level1");
+  // console.log("Namespace list", nsData);
 
-  socket
-    .to("level1")
-    .emit("joined", `${socket.id} says have joined the level 1 room!`);
-
-  // io.of("/")
-  //   .to("level1")
-  //   .emit("joined", `${socket.id} says have joined the level 1 room!`);
+  // send the nsData back to the client.  We need to use socket, NOT io, because we want it to go to just this client.
+  socket.emit("nsList", nsData);
 });
 
-io.of("/admin").on("connection", socket => {
-  console.log("Someone connected to the admin namespace");
-  io.of("/admin").emit("welcome", "Welcome to the admin channel!");
+// Loop through each "individual" namespaces and listen for a connection to the endpoint
+namespaces.forEach(namespace => {
+  console.log("Individual namespace", namespace);
+  io.of(namespace.endpoint).on("connection", socket => {
+    console.log(`${socket.id} has joined ${namespace.endpoint}`);
+  });
 });
